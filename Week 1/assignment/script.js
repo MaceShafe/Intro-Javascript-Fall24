0806477b568e26e65bfa80add7345bd4e7ebea02
MaceShafe <142840758+MaceShafe@users.noreply.github.com>
2024-08-28 12:26:33 -0500

    week 1 assignment

---
+++ Week 1/assignment/script.js
@@ -0,0 +1,3 @@
+let var1 = 'images/IDE.jpg'
+let var2 = 'images/GIT.jpg'
+let var3 = '<p>This text has been added from the script.js file!</p>'
---
+++ Week 1/videopractice/script.js
@@ -0,0 +1,18 @@
+
+// window.alert("Hi, I am an alert from the BOM");
+// document.write("I'm text to the DOM")
+
+// let x = 2;
+// let y = 3;
+// let z;
+
+// x = 5;
+
+// z = x + y;
+// document.write("The value of Z is "+z);
+
+// z=z**2
+
+// document.write("<p>The value of Z is now "+z);
+
+    let myImageNode = document.getElementById('imageToChange')
