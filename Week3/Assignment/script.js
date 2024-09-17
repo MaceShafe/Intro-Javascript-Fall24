588b90bb057b051571d7149b7a0b5594f5fd38ae
MaceShafe <142840758+MaceShafe@users.noreply.github.com>
2024-09-17 02:09:56 -0500

    [COMPLETE W3] fixed up checking issues!

--- Week3/Assignment/script.js
+++ Week3/Assignment/script.js
@@ -8,6 +8,7 @@ const GRIDSIZE=3;
 let xMark = "X";
 let oMark = "O";
 let lastMark = oMark;
+let mark = "";
 let boardArray = new Array(GRIDSIZE); // Array with initial size of GRIDSIZE
 
 // references
@@ -42,9 +43,9 @@ console.log('initialized!')
     let htmlCode = '';
     let colSize = Math.round(12/GRIDSIZE);
 
-        for (let row = 1; row<=GRIDSIZE; row++){
+        for (let row = 0; row<GRIDSIZE; row++){
         htmlCode += '<div class="row h-25">';
-            for (let col=1; col<=GRIDSIZE; col++){
+            for (let col=0; col<GRIDSIZE; col++){
             htmlCode += '<div onClick="markIt('+row+','+col+')" id="'+row+'_'+col+'" class="col-'+colSize+' h-100 bg-primary bg-gradient border border-info shadow d-flex align-items-center justify-content-center fs-1">'+boxNumber+++'('+row+','+col+')</div>';
             // you can clean up the output of that html if you want to :-)
         }
@@ -56,53 +57,97 @@ console.log('initialized!')
 
 // ****************************************************************
 function markIt(row,col) {
-let mark = ""
     //? if the lastMark is X, then set mark=O, else set mark=X
     if(lastMark == "X") {
         mark = "O"
-        lastMark = "O"
     } else {
         mark="X"
-        lastMark = "X"
     }
 
+    lastMark = mark;
 
     document.getElementById(row+'_'+col).innerHTML = mark;
 
     //? save the mark in boardArray multidimensional array  (remember Array indexes start at 0 and not 1)
 
-boardArray [row-1] [col-1] = mark; //literally just un-offsetting it because its broken otherwise and i dont know wh
+    boardArray[row][col] = mark;
 
-console.log(row);
-console.log(col);
-console.log(boardArray);
+
+    console.log(boardArray);
 
 
     // check if there's a winner
-    let winner = checkBoard();
+    let winner = checkBoard(mark);
     if (winner!="") {
         alert("Game Over! "+winner);
     }
 }
 
 // ****************************************************************
-function checkBoard() {
+function checkBoard(Mark) {
 
+let Test = true; //Assumes column is winner
 
-for (col=0; col<GRIDSIZE; col++) {  // loop through all columns
-        for (row=0; row<GRIDSIZE; row++) { // loop through all rows
-         
-            if (boardArray[row][0] === boardArray[row][1] && boardArray[row][1] === boardArray[row][2] && boardArray[row][0]!==null  && boardArray[row][1]!==null && boardArray[row][0]!==null  && boardArray[row][2]!==null)
-                 return boardArray[row][0] + 'wins horisontally!';
-            else 
-                return "";
+// *COLUMNS
+    for (col = 0; col < GRIDSIZE; col++) {  // loop through all columns
+        let Test = true; //ensures reset
+        Mark = boardArray[0][col]
+        for (row = 0; row < GRIDSIZE; row++) { // loop through all rows
+            if (boardArray[row][col] != Mark) {
+                Test = false; //if the column is not the same, it doesnt go through
+                break;
+            }
 
         }
-        // if (xTest===true) {
-        //     return xMark+" is the vertical winner!";
-        // }
-        // return ("");
+        if (Test === true && Mark != undefined) {
+            return Mark + " is the vertical winner at column" + (col + 1) + "!"
+        }
     }
+
+//* ROWS
+    for (row = 0; row < GRIDSIZE; row++) {  // loop through all rows
+        let Test = true; //ensures reset
+        Mark = boardArray[row][0]
+        for (col = 0; col < GRIDSIZE; col++) { // loop through all cols
+            if (boardArray[row][col] != Mark) {
+                Test = false; //if the column is not the same, it doesnt go through
+                break;
+            }
+
+        }
+        if (Test === true && Mark != undefined) {
+            return Mark + " is the vertical winner at row" + (row + 1) + "!"
+        }
+    }
+
+//* LEFT RIGHT DIAGONALS
+    Test = true; //resets test 
+    Mark = boardArray[0][0]; //sets Mark to be the top left square 
+    for (col = 0, row = 0; col < GRIDSIZE && row < GRIDSIZE; col++, row++) {  // loop through diagonal left to right
+        if (boardArray[row][col] != Mark) { 
+            Test = false; //if its  not the same, it doesnt go through
+            break;
+        }
+    }
+    if (Test === true && Mark != undefined) { 
+        return Mark + " is the diagonal left to right winner!"
+    }
+//* RIGHT LEFT DIAGONALS
+    Test = true; //resets test 
+    Mark = boardArray[0][GRIDSIZE-1]; //sets Mark to be the top left square 
+    for (col = GRIDSIZE-1, row = 0; col >= 0 && row <GRIDSIZE; col--, row++){  // loop through diagonal left to right
+        console.log(row)
+        console.log(col)
+        if (boardArray[row][col] != Mark) { 
+            Test = false; //if its  not the same, it doesnt go through
+            break;
+        }
+    }
+    if (Test === true && Mark != undefined) { 
+        return Mark + " is the diagonal right to left winner!"
+    }
+
+return ""
 }
 // ****************************************************************
 
